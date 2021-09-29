2c6668b18040196c0d27bb0961a0da15827bf049
Kingstalux <kingsleyngu25@gmail.com>
2021-09-29 10:55:38 +0100

    Style to do list

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -22,8 +22,22 @@ const tasks = [
 ];
 
 tasks.forEach((element) => {
+  const div = document.createElement('div');
+  div.className = 'task';
+  const container = document.getElementById('list-container');
+  container.appendChild(div);
+  const div2 = document.createElement('div');
+  div2.className = 'div';
+  div.appendChild(div2);
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  div2.appendChild(checkbox);
   const para = document.createElement('p');
   para.innerText = element.description;
-  const container = document.getElementById('list-container');
-  container.appendChild(para);
+  div2.appendChild(para);
+  const icon = document.createElement('i');
+  icon.className = 'ellipsis vertical icon';
+  div.appendChild(icon);
+  const hr = document.createElement('hr');
+  container.appendChild(hr);
 });
