0a0da9decd24a9a5f8606000c9ff0e24bc21c561
Kingstalux <kingsleyngu25@gmail.com>
2021-09-29 07:11:13 +0100

    Set up linters

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
     }),
   ],
   output: {
@@ -25,4 +25,4 @@ module.exports = {
       },
     ],
   },
- };
+};
