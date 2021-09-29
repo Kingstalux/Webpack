ad5d43ea44307e3bef907419279ac5de0abc2495
Kingstalux <kingsleyngu25@gmail.com>
2021-09-29 17:37:29 +0100

    Edit styles for checkbox

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // import _ from 'lodash';
 import './style.css';
+import { status } from'./script.js';
 
 const tasks = [
   {
@@ -31,13 +32,27 @@ tasks.forEach((element) => {
   div.appendChild(div2);
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
+  checkbox.className = 'check'
   div2.appendChild(checkbox);
   const para = document.createElement('p');
   para.innerText = element.description;
   div2.appendChild(para);
+  checkbox.addEventListener('click', () => {
+    status(checkbox, element)
+    if (checkbox.checked == true) {
+      para.classList.add('cancel')
+      // element.completed = true
+      console.log(element);
+    } else {
+      para.classList.remove('cancel')
+      // element.completed = false
+      console.log(element);
+    }
+  })
   const icon = document.createElement('i');
   icon.className = 'ellipsis vertical icon';
   div.appendChild(icon);
   const hr = document.createElement('hr');
   container.appendChild(hr);
 });
+
---
+++ src/script.js
@@ -0,0 +1,7 @@
+// export function status (checkbox, element) {
+//     if (checkbox.checked == true) {
+//         element.completed = true
+//       } else {
+//         element.completed = false
+//       }
+// }
