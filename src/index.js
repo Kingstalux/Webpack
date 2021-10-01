b6cd6b21fac7cf354cc4d20994c184c09284c0ce
Kingstalux <kingsleyngu25@gmail.com>
2021-10-01 05:33:20 +0100

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 // import _ from 'lodash';
 import './style.css';
 import status from './script.js';
-import { addItem } from './script2.js';
-import { editItem } from './script2.js';
-import { removeItem } from './script2.js';
-import { clearCompleted } from './script2.js';
+import {
+  addItem, editItem, removeItem, clearCompleted,
+} from './script2.js';
 
 let taskList = JSON.parse(localStorage.getItem('tasks'));
 
@@ -13,112 +12,105 @@ if (taskList === null) {
 }
 
 const display = (element) => {
-    const div = document.createElement('div');
-    div.className = 'task';
-    div.id = element.index
-    const container = document.getElementById('list-container');
-    container.appendChild(div);
-    const div2 = document.createElement('div');
-    div2.className = 'div';
-    div.appendChild(div2);
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.className = 'check';
-    div2.appendChild(checkbox);
-    const para = document.createElement('p');
-    para.innerText = element.description;
-    div2.appendChild(para);
-    checkbox.addEventListener('change', () => {
-      status(checkbox, element, taskList);
-      if (checkbox.checked === true) {
-        para.classList.add('cancel');
-        localStorage.setItem('tasks', JSON.stringify(taskList));
-        taskList = JSON.parse(localStorage.getItem('tasks'));
-      } else {
-        para.classList.remove('cancel');
-        localStorage.setItem('tasks', JSON.stringify(taskList));
-        taskList = JSON.parse(localStorage.getItem('tasks'));
-      }
-    });
-    if (element.completed === true) {
+  const div = document.createElement('div');
+  div.className = 'task';
+  const container = document.getElementById('list-container');
+  container.appendChild(div);
+  const div2 = document.createElement('div');
+  div2.className = 'div';
+  div.appendChild(div2);
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.className = 'check';
+  div2.appendChild(checkbox);
+  const para = document.createElement('p');
+  para.innerText = element.description;
+  div2.appendChild(para);
+  checkbox.addEventListener('change', () => {
+    status(checkbox, element, taskList);
+    if (checkbox.checked === true) {
       para.classList.add('cancel');
-      checkbox.checked = true;
-    } else {
-      para.classList.remove('cancel');
-      checkbox.checked = false;
-    }
-    const icon = document.createElement('i');
-    icon.className = 'ellipsis vertical icon check2';
-
-    const spam = document.createElement('spam')
-    spam.appendChild(icon)
-    div.appendChild(spam);
-
-    const input = document.createElement('input')
-    input.type = 'text'
-    input.className = 'edit-item hidden'
-    div.appendChild(input)
-
-
-    icon.addEventListener('click', () => {
-      editItem(spam, spam2, input, para, element)
-    })
-
-    input.addEventListener('keyup', function(event) {
-      if (event.keyCode === 13) {
-        event.preventDefault();
-        element.description = input.value
-        // para.innerText = element.description
-        localStorage.setItem('tasks', JSON.stringify(taskList));
-        taskList = JSON.parse(localStorage.getItem('tasks')); 
-        window.location.reload()
-        // spam.classList.remove('hidden')
-        // spam2.classList.add('hidden')
-        // input.classList.add('hidden')
-        // para.classList.remove('hidden')
-       }
-    })   
-
-    const icon2 = document.createElement('i');
-    icon2.className = 'trash alternate outline icon check';
-    icon2.addEventListener('click', () => {
-      removeItem(element, taskList)
       localStorage.setItem('tasks', JSON.stringify(taskList));
       taskList = JSON.parse(localStorage.getItem('tasks'));
-      div.remove()
-      hr.remove()
-      // taskList.forEach(element => {
-      //   div.id = element.index
-      // });
-    })
-    const spam2 = document.createElement('spam')
-    spam2.className = 'hidden'
-    spam2.appendChild(icon2)
-    div.appendChild(spam2);
-    const hr = document.createElement('hr');
-    container.appendChild(hr);
-}
+    } else {
+      para.classList.remove('cancel');
+      localStorage.setItem('tasks', JSON.stringify(taskList));
+      taskList = JSON.parse(localStorage.getItem('tasks'));
+    }
+  });
+  if (element.completed === true) {
+    para.classList.add('cancel');
+    checkbox.checked = true;
+  } else {
+    para.classList.remove('cancel');
+    checkbox.checked = false;
+  }
+  const icon = document.createElement('i');
+  icon.className = 'ellipsis vertical icon check2'
+  const spam = document.createElement('spam');
+  spam.appendChild(icon);
+  div.appendChild(spam);
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.className = 'edit-item hidden';
+  div.appendChild(input);
 
-taskList.forEach(element => {
-  display(element)
+  input.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      element.description = input.value;
+      // para.innerText = element.description
+      localStorage.setItem('tasks', JSON.stringify(taskList));
+      taskList = JSON.parse(localStorage.getItem('tasks'));
+      window.location.reload();
+      // spam.classList.remove('hidden')
+      // spam2.classList.add('hidden')
+      // input.classList.add('hidden')
+      // para.classList.remove('hidden')
+    }
+  });
+
+  const icon2 = document.createElement('i');
+  icon2.className = 'trash alternate outline icon check';
+  const spam2 = document.createElement('spam');
+  spam2.className = 'hidden';
+  spam2.appendChild(icon2);
+  div.appendChild(spam2);
+  const hr = document.createElement('hr');
+  container.appendChild(hr);
+
+  icon.addEventListener('click', () => {
+    editItem(spam, spam2, input, para, element);
+  });
+
+  icon2.addEventListener('click', () => {
+    removeItem(element, taskList);
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+    taskList = JSON.parse(localStorage.getItem('tasks'));
+    div.remove();
+    hr.remove();
+  });
+};
+
+taskList.forEach((element) => {
+  display(element);
 });
 
 document.getElementById('enter-icon').addEventListener('click', () => {
-  addItem(taskList, display)
-  document.querySelector('.new-item').value = ''
+  addItem(taskList, display);
+  document.querySelector('.new-item').value = '';
   localStorage.setItem('tasks', JSON.stringify(taskList));
   taskList = JSON.parse(localStorage.getItem('tasks'));
-})
+});
 
-document.querySelector('.new-item').addEventListener('keyup', function(event) {
+document.querySelector('.new-item').addEventListener('keyup', (event) => {
   if (event.keyCode === 13) {
     event.preventDefault();
-    document.getElementById('enter-icon').click()
-   }
-})
+    document.getElementById('enter-icon').click();
+  }
+});
 
 document.querySelector('.button').addEventListener('click', () => {
-  clearCompleted(taskList)
-  window.location.reload()
-})
-
+  clearCompleted(taskList);
+  window.location.reload();
+});
--- src/script2.js
+++ src/script2.js
@@ -1,39 +1,39 @@
-export function addItem (taskList, display) {
-    const newItem = document.querySelector('.new-item').value
-    let i = taskList.length
-    const element = {
-        description: newItem,
-        completed: false,
-        index: i,
+export function addItem(taskList, display) {
+  const newItem = document.querySelector('.new-item').value;
+  const i = taskList.length;
+  const element = {
+    description: newItem,
+    completed: false,
+    index: i,
+  };
+  taskList.push(element);
+  display(element);
+}
+
+export function editItem(spam, spam2, input, para, element) {
+  spam.classList.add('hidden');
+  spam2.classList.remove('hidden');
+  input.classList.remove('hidden');
+  para.classList.add('hidden');
+  input.value = element.description;
+  // document.querySelector('.task').classList.add('color')
+}
+
+export function removeItem(element, taskList) {
+  const i = element.index;
+  taskList.splice(i, 1);
+  taskList.forEach((element) => {
+    element.index = taskList.indexOf(element);
+  });
+}
+
+export function clearCompleted(taskList) {
+  taskList = taskList.filter((element) => {
+    if (element.completed !== true) {
+      return true;
     }
-    taskList.push(element)
-    display(element)
-}
-
-export function editItem (spam, spam2, input, para, element) {
-    spam.classList.add('hidden')
-    spam2.classList.remove('hidden')
-    input.classList.remove('hidden')
-    para.classList.add('hidden')
-    input.value = element.description
-    // document.querySelector('.task').classList.add('color')
-}
-
-export function removeItem (element, taskList) {
-    let i = element.index
-    taskList.splice(i, 1)
-    taskList.forEach(element => {
-        element.index = taskList.indexOf(element)
-    }); 
-}
-
-export function clearCompleted (taskList) {
-    taskList = taskList.filter((element) => {
-        if (element.completed !== true) {
-          return true;
-        }
-        return false;
-      });
-      localStorage.setItem('tasks', JSON.stringify(taskList));
-      taskList = JSON.parse(localStorage.getItem('tasks'));
+    return false;
+  });
+  localStorage.setItem('tasks', JSON.stringify(taskList));
+  taskList = JSON.parse(localStorage.getItem('tasks'));
 }
