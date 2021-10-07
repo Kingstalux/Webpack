db343879b356837579ef6ac7a463d7aa72a2b926
Kingstalux <kingsleyngu25@gmail.com>
2021-10-07 16:32:57 +0100

    Fix linters errors

--- src/_tests_/edit.test.js
+++ src/_tests_/edit.test.js
@@ -2,41 +2,39 @@
  * @jest-environment jsdom
  */
 
- import localStorageMock from '../_mocks_/localStorage.js';
- import editItem from '../_mocks_/edit.js';
- 
- describe('Edit tasks descriptions', () => {
-   const taskList = [
-     {
-       description: 'Breaking Bad',
-       completed: false,
-       index: 1,
-     },
+import localStorageMock from '../_mocks_/localStorage.js';
+import editItem from '../_mocks_/edit.js';
 
-     {
-        description: 'Squid games',
-        completed: false,
-        index: 2,
-      },
-   ];
- 
-   test('change the description of Breaking Bad to Game of Thrones', () => {
-     const element = taskList[0];
-     document.body.innerHTML = '<input class="edit-item" value="Game of Thrones">';
-     editItem(element, taskList)
-     expect(element.description).toBe('Game of Thrones');
-   });
- 
-   test('Local storage should not change', () => {
-     expect(localStorageMock.getItem('data')).toHaveLength(2);
-   });
+describe('Edit tasks descriptions', () => {
+  const taskList = [
+    {
+      description: 'Breaking Bad',
+      completed: false,
+      index: 1,
+    },
 
-   test('change the description of Squid games to Into the badlands', () => {
+    {
+      description: 'Squid games',
+      completed: false,
+      index: 2,
+    },
+  ];
+
+  test('change the description of Breaking Bad to Game of Thrones', () => {
+    const element = taskList[0];
+    document.body.innerHTML = '<input class="edit-item" value="Game of Thrones">';
+    editItem(element, taskList);
+    expect(element.description).toBe('Game of Thrones');
+  });
+
+  test('Local storage should not change', () => {
+    expect(localStorageMock.getItem('data')[0].description).toBe('Game of Thrones');
+  });
+
+  test('change the description of Squid games to Into the badlands', () => {
     const element = taskList[1];
     document.body.innerHTML = '<input class="edit-item" value="Into the badlands">';
-    editItem(element, taskList)
+    editItem(element, taskList);
     expect(element.description).toBe('Into the badlands');
   });
- 
- });
- 
+});
--- src/_tests_/status.test.js
+++ src/_tests_/status.test.js
@@ -2,11 +2,11 @@
  * @jest-environment jsdom
  */
 
- import localStorageMock from '../_mocks_/localStorage.js';
- import status from '../_mocks_/status.js';
- 
- describe('Change the completed key of tasks', () => {
-   const taskList = [
+import localStorageMock from '../_mocks_/localStorage.js';
+import status from '../_mocks_/status.js';
+
+describe('Change the completed key of tasks', () => {
+  const taskList = [
     {
       description: 'Into the badlands',
       completed: true,
@@ -20,24 +20,23 @@
     },
   ];
 
-   test('change the completed key of the first element to false', () => {
-     const element = taskList[0]
-     const checkbox = document.createElement('checkbox')
-    checkbox.checked = false
-     status(checkbox, element, taskList) 
-     expect(element.completed).toBe(false);
-   });
- 
-   test('completed key of first element should change to false in local storage', () => {
-     expect(localStorageMock.getItem('data')[0].completed).toBe(false);
-   });
+  test('change the completed key of the first element to false', () => {
+    const element = taskList[0];
+    const checkbox = document.createElement('checkbox');
+    checkbox.checked = false;
+    status(checkbox, element, taskList);
+    expect(element.completed).toBe(false);
+  });
 
-   test('change the completed key of the second element to true', () => {
-    const element = taskList[1]
-    const checkbox = document.createElement('checkbox')
-    checkbox.checked = true
-    status(checkbox, element, taskList) 
+  test('completed key of first element should change to false in local storage', () => {
+    expect(localStorageMock.getItem('data')[0].completed).toBe(false);
+  });
+
+  test('change the completed key of the second element to true', () => {
+    const element = taskList[1];
+    const checkbox = document.createElement('checkbox');
+    checkbox.checked = true;
+    status(checkbox, element, taskList);
     expect(element.completed).toBe(true);
   });
- });
- 
+});
